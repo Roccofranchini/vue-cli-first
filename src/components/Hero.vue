f5e7c14ec7a3c468ffc08500c9c29664a4340a04
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-03 16:03:40 +0200

    Title-Hero-added

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="jumbo">
+		<span>Hero</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Hero",
+	props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jumbo {
+	margin-top: 20px;
+	height: 300px;
+	width: 100%;
+	border: 1px solid black;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #abed;
+}
+.jumbo span {
+	color: pink;
+	font-size: 40px;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<span>TITOLO</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Title",
+	props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+span {
+	color: wheat;
+	font-size: 50px;
+	background-color: black;
+}
+</style>
